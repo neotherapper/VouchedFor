3b1909e6f5a39cf6a8927da4017cdd143e5a733a
neotherapper <george.pilitsoglou@gmail.com>
2014-05-20 16:00:11 +0200

    changed profile

--- node/socket.js
+++ node/socket.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:4000');
+var socket = io.connect();
 socket.on('news', function (data) {
 	console.log(data);
 	socket.emit('my other event', { my: 'data' });
